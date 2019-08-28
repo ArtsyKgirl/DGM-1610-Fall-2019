b480fb4096009e985a4a09b15fa40145931c69e6
Kirsten <1086969@my.uvu.edu>
2019-08-28 17:23:03 -0600

    Added Working Unity Project

---
+++ DMG 1610 fall 2019/Assets/Scripts/Assignments/Variables.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    string firstName="Kirsten";
+    int age = 18;
+    float height = 5.1f;
+    bool married = false;
+
+
+    
+    void Start()
+    {
+        Debug.Log("First Name: " + firstName);
+        Debug.Log("Age: " + age);
+        Debug.Log("Height: " + height);
+        Debug.Log("Married: " + married);
+    }
+
+  
+}
