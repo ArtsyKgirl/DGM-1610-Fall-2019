6e9e14f626659852b42c51ea4df7cbc2bc4d9ac2
Kirsten <1086969@my.uvu.edu>
2019-09-04 18:44:07 -0600

    Added new Functions

--- DMG 1610 fall 2019/Assets/Scripts/Assignments/Functions.cs
+++ DMG 1610 fall 2019/Assets/Scripts/Assignments/Functions.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Functions : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class Functions : MonoBehaviour
     // string lastName;
     // int age;
 
+    //Attributes
     int num1;
     int num2;
     int total;
@@ -22,16 +24,66 @@ public class Functions : MonoBehaviour
         // age = 90;
 
         // Customer(firstName, lastName, age);
-        CalcAdd(11,23);
+        CalcAdd(11, 23);
+        CalcSub(23, 15);
+        CalcDivide(25, 5);
+        CalcMulti(8, 7);
+        CalcSqr(6, 2);
+        CalcRoot(9);
 
     }
 
-   int CalcAdd (int n1, int n2)
+   int CalcAdd(int n1, int n2)
     {
-        Debug.Log("Number 1 =" + n1+" Number 2 =" + n2);
+        Debug.Log("Number 1 = " + n1 + " Number 2 = " + n2);
         total = n1 + n2;
         Debug.Log(total);
         
+        return total;
+    }
+
+    int CalcSub(int n1, int n2)
+    {
+        Debug.Log("Number 1 = " + n1 + " Number 2 = " + n2);
+        total = n1 - n2;
+        Debug.Log(total);
+
+        return total;
+    }
+
+    int CalcDivide(int n1, int n2)
+    {
+        Debug.Log("Number 1 = " + n1 + " Number 2 = " + n2);
+        total = n1 / n2;
+        Debug.Log(total);
+
+        return total;
+    }
+
+    int CalcMulti(int n1, int n2)
+    {
+        Debug.Log("Number 1 = " + n1 + " Number 2 = " + n2);
+        total = n1 * n2;
+        Debug.Log(total);
+
+        return total;
+    }
+
+    int CalcSqr(int n1, int n2)
+    {
+        Debug.Log("Number 1 = " + n1 + " Number 2 = " + n2);
+        total = (int)Math.Pow(n1, n2);
+        Debug.Log(total);
+
+        return total;
+    }
+
+    int CalcRoot(int n1)
+    {
+        Debug.Log("Number = " + n1);
+        total = (int)Math.Sqrt(n1);
+        Debug.Log(total);
+
         return total;
     }
 
