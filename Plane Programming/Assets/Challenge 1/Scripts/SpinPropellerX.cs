be6debc6ef0c1b3cdac538eecf44fe8938ce363a
Kirsten <1086969@my.uvu.edu>
2019-09-23 16:45:18 -0600

    Added Plane Challenge

---
+++ Plane Programming/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    private float rotationSpeed = 1000;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotation of Propeller
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
