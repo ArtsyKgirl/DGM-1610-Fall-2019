f174e248d3d4e89e16f2a657ae8b1ff21d480533
Kirsten <1086969@my.uvu.edu>
2019-10-28 17:01:22 -0600

    Add Maze and Added effect

---
+++ A Maze/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float speed = 20f;
+    private float turnSpeed = 40f;
+    private float horizontalInput;
+    private float forwardInpute;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInpute = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInpute);
+        transform.Rotate(Vector3.down, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
